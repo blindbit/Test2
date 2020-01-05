67a6be00dcd6ff03a18d556252a511dc9bf8cc7e
Kobayashi <Kobayashi@Kobayashis-MacBook-Pro.local>
2020-01-05 17:46:21 +0100

    Second Commit

---
+++ front.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+
+# name = input("Your Name? ")
+# print("Hello,", name)
+
+# print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_great):
+    greeting = "Hello, {}".format(who_to_great)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
